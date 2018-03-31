41edb1254ff53cbbbf4a7cef1ce76c0721ba7388
Alice1820 <18001208205@163.com>
2018-03-31 11:24:30 +0800

    Complete dataset

--- dataset.py
+++ dataset.py
@@ -8,12 +8,53 @@ import scipy.io as sio
 
 class smallNORB(Dataset):
     def __init__(self, root, split='train'):
+
         with open('data/' + split + '.pkl', 'rb') as f:
             self.data = pickle.load(f)
     
         self.root_dir = root
         self.img_dir = os.path.join(root, 'smallnorb_' + split)
     
-        self.	
-  
-    try push from git2go
+		self.transform = transforms.Compose([
+									Scale(96, 96),
+									transforoms.ToTensor()
+									])
+		self.transform_aug = transforms.Compose([
+									Scale(96, 96),
+									transforms.Pad(3),
+									transforms.RandomCrop([96, 96]),
+									transforoms.ToTensor()
+									
+		self.if_aug = (split==‘train’)
+
+		self.split = split
+
+
+	def __getitem__(self, index):
+
+		imgfile, label = self.data[index]
+		
+		# dir_path = os.path.join(self.root_dir, ‘smallnorb_’ + self.split, label, label + )
+		img_path = os.path.join(self.root_dir, imgfile)
+		img = imread(img_path)
+		if self.if_aug:
+			img = self.transform_aug(img)
+		else:
+			img = self.transform(img)
+
+		return img, label
+
+	def __len__(self):
+
+		return len(self.data)
+
+
+	def collate_data(batch):
+	
+		imgs, labels = [], []
+		batch_size = len(batch)
+
+		for i, (img, label) in enumerate(batch):
+			imgs.append(img)
+			labels.append(label)
+		return torch.stack(imgs)m torch.LongTensor(labels)
